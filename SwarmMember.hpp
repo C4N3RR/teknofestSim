6686a8b80d22afb90b4a98ea9a45ad65876b15fb
SoutTabHi <78692299+C4N3RR@users.noreply.github.com>
2026-04-18 20:06:47 +0300

    Update SwarmMember.hpp with new content.

--- SwarmMember.hpp
+++ SwarmMember.hpp
@@ -1,6 +1,6 @@
 #pragma once
 
-#include "Common.hpp" // Takımın ortak struct'ları (varsa)
+#include "Common.hpp"
 #include <mavsdk/mavsdk.h>
 #include <mavsdk/plugins/offboard/offboard.h>
 #include <mavsdk/plugins/action/action.h>
@@ -11,28 +11,21 @@
 
 class SwarmMember {
 public:
-    // Kurucu fonksiyon (Senin ConnectionHandler'dan göndereceğin system objesini alır)
     SwarmMember(std::shared_ptr<mavsdk::System> system, int id);
 
-    // Arm() -> Takeoff() -> Offboard moduna geçiş
     bool prepare_for_mission();
 
-    // SwarmBrain'den gelen hedefleri (NED formatında) MAVSDK'ya basar
     void update_target(Eigen::Vector3d target_ned, float yaw_deg);
 
-    // Acil durum veya iniş için
     void land();
 
-    // Telemetri verisi çekmek için
     mavsdk::Telemetry::Position get_current_pos();
 
-    // İHA ID'sini döndürmek için yardımcı fonksiyon
     int get_id() const { return _id; }
 
 private:
     std::shared_ptr<mavsdk::System> _system;
     
-    // MAVSDK eklentileri (Plugins)
     std::unique_ptr<mavsdk::Offboard> _offboard;
     std::unique_ptr<mavsdk::Action> _action;
     std::unique_ptr<mavsdk::Telemetry> _telemetry;
